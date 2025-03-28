5e60841fbecbb963ad6d150016760e667f8b7642
Simon Verhoeven <verhoeven.simon@gmail.com>
2025-03-28 19:58:22 +0100

    feature: add Structured Concurrency demo

---
+++ src/main/java/dev/simonverhoeven/java25demo/preview/StructuredConcurrency.java
@@ -0,0 +1,46 @@
+package dev.simonverhoeven.java25demo.preview;
+
+import java.util.concurrent.StructuredTaskScope;
+import java.util.function.Supplier;
+
+/// JEP 8340343: Structured Concurrency (Fifth Preview)
+
+public class StructuredConcurrency {
+
+    public GarderobeSelectionInput composeGarderobeSelectionInput(String userId) {
+        try (final var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            Supplier<Person> personTask =
+                    scope.fork(() -> findPerson(userId));
+            Supplier<Weather> weatherTask =
+                    scope.fork(this::fetchWeather);
+            Supplier<Activity> activityTask =
+                    scope.fork(() -> findActivity(userId));
+            scope.join();
+
+            final var person = personTask.get();
+            final var weather = weatherTask.get();
+            final var activity = activityTask.get();
+
+            return new GarderobeSelectionInput(person, weather, activity);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Person findPerson(String id) throws InterruptedException {
+        Thread.sleep(2000L);
+        return null;}
+    Weather fetchWeather() throws InterruptedException {
+        Thread.sleep(5000L);
+        return null;}
+    Activity findActivity(String id) throws InterruptedException {
+        Thread.sleep(3000L);
+        return null;
+    }
+
+    class Person {}
+    class Weather {}
+    class Activity {}
+    public record GarderobeSelectionInput(Person person, Weather weather, Activity activity){}
+}
