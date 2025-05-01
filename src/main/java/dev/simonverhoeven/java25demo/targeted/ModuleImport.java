07be3bd30d22c563dd56d93c29ea3b531ae2bc9a
Simon Verhoeven <verhoeven.simon@gmail.com>
2025-05-01 20:41:03 +0200

    docs: add module import (511)

---
+++ src/main/java/dev/simonverhoeven/java25demo/targeted/ModuleImport.java
@@ -0,0 +1,15 @@
+package dev.simonverhoeven.java25demo.targeted;
+
+import module java.base;
+
+import static java.lang.System.out;
+
+/// JEP 511: Module Import Declarations
+
+public class ModuleImport {
+
+    public static void main(String[] args) {
+        List<String> elements = List.of("One", "two", "THREE");
+        elements.stream().map(String::toUpperCase).forEach(out::println);
+    }
+}
