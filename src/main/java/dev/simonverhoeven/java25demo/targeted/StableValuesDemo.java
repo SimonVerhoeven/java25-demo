725221d63eee545e8aeb342412217eaa584832bd
Simon Verhoeven <verhoeven.simon@gmail.com>
2025-06-05 19:17:30 +0200

    feature: add stable values demo

--- src/main/java/dev/simonverhoeven/java25demo/targeted/StableValuesDemo.java
+++ src/main/java/dev/simonverhoeven/java25demo/targeted/StableValuesDemo.java
@@ -1,8 +1,25 @@
 package dev.simonverhoeven.java25demo.targeted;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.Supplier;
+
 /// JEP-502: Stable Values (Preview)
 
 public class StableValuesDemo {
 
-    // TODO
+    // We defer the initialization, it'll be initialized at-most once and is immutable.
+    // This enables constant-folding.
+    private final Supplier<Integer> meaningOfLife = StableValue.supplier(() -> 42);
+    private final List<BigDecimal> numbers = StableValue.list(42, BigDecimal::valueOf);
+
+    Integer getMeaningOfLife() {
+        // The constant is computed on the first get invocation
+        return meaningOfLife.get();
+    }
+
+    List<BigDecimal> getNumbers() {
+        // the constant computation is deferred to the first .get(index) or similar.
+        return numbers;
+    }
 }
